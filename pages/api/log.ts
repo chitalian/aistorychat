23d4cb6966126d229daec9f32fc415f535bb6434
Justin <justintorre75@gmail.com>
2022-12-08 01:03:10 -0500

    remove supabase

--- pages/api/log.ts
+++ pages/api/log.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { supabaseServer } from "../../lib/supabaseServer";
+// import { supabaseServer } from "../../lib/supabaseServer";
 
 export interface promptsDB {
   id: string;
