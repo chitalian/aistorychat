d1eaf0a2536c102680d823b1f08b87f55a1a32ad
Justin <justintorre75@gmail.com>
2022-12-09 03:22:00 -0500

    pretty images

--- pages/api/queueImage.ts
+++ pages/api/queueImage.ts
@@ -21,7 +21,7 @@ export default async function handler(
   const body = {
     version: "27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
     input: {
-      prompt: prompt,
+      prompt: `${prompt}, intricate details, shot on portra 400, film grain, color photo, cinematic lighting, 4k`,
       negative_prompt:
         // credit https://www.reddit.com/r/StableDiffusion/comments/zdbi1u/robot_selfies_redshift_diffusion_786/
         "nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet ,cropped,poorly drawn hands,poorly drawn face,mutation,deformed,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,extra fingers,fewer digits,extra limbs,extra arms,extra legs,malformed limbs,fused fingers, too many fingers, long neck, cross-eyed",
